ceb4b8fcc716793c26d1ca76e2289f12ad3a8265
Fabrice <fabwild@dejeammes.com>
2017-12-10 19:35:24 +0100

    add ok, wiki ok

--- controllers/Indexes.js
+++ controllers/Indexes.js
@@ -1,8 +1,9 @@
 const request = require('request');
-// const fetch = require('node-fetch');;
-const cheerio = require('cheerio');
-// const CircularJSON = require('circular-json')
-const util = require('util');
+const models = require("../models");
+const uuidv1 = require('uuid/v1');
+
+//returns an integer between 0 and n, included
+function randrank(n) {return Math.floor(Math.random()*n)};
 
 var Indexes = {
 
@@ -11,34 +12,62 @@ var Indexes = {
     res.render('index/index', { title: 'Express' });
   },
 
-  // all current wikiquote quotes
-  wikiquoteGet: function (req, res, next) {
+  addwireGet: function (req, res, next) {
+    let newid;
+    let count = 0;
     request("https://en.wikiquote.org/api/rest_v1/page/html/The_Wire?redirect=false", function(error, response, body){
-      // console.log(response);
-      let $ = cheerio.load(response.body);
-      let quotes = $('dl');
+      let matches = response.body.match(new RegExp('<dl(.+?)/dl>','gi'));
+      for (var i = 0; i < matches.length; i++) {
+        newid = uuidv1();
+        models.Quote.create(
+          {
+            uuid: newid,
+            text: matches[i]
+          })
+          // .spread(
+          //   (quote, created) => {
+          //   created ? count++ : res.send('error');
+          // })
+        }
+      });
+    console.log(count);
+    res.send(count);
 
+    },
 
-      // console.log(quotes.length);
+    quoteGet: function (req, res, next) {
       let data;
-      if(req.query.all == 'true'){
-        console.log('all');
-        data = util.inspect(quotes);
-      } else
-      {
-        let randrank = Math.round(Math.random()*quotes.length);
-        console.log(randrank);
-        data = util.inspect(quotes[randrank]);
-      }
-      // console.log(data);
-      res.json(data);
-    });
-  },
+      models.Quote.findAll({})
+      .then(results => {
+        // console.log(results[0].dataValues.text);
+        data = results[randrank(results.length)].dataValues.text;
+        console.log(data);
+        res.send(data);
+      });
+    },
 
-  erreur404: function(req, res){
-    res.render('error');
-  },
+    // a quote from current wikiquote, for all quotes : ?all=true
+    wikiquoteGet: function (req, res, next) {
+      request("https://en.wikiquote.org/api/rest_v1/page/html/The_Wire?redirect=false",
+      function(error, response, body){
+        // console.log(response);
+        // let $ = cheerio.load(response.body);
+        // let quotes = $('dl');
+        let matches = response.body.match(new RegExp('<dl(.+?)/dl>','gi'));
 
-};
+        let data;
+        req.query.all ?
+        data = matches
+        :data = matches[randrank(matches.length)];
+        // res.json(data);
+        res.send(data);
+      });
+    },
 
-module.exports = Indexes;
+    erreur404: function(req, res){
+      res.render('error');
+    },
+
+  };
+
+  module.exports = Indexes;
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,12 @@ router.get('/', indexes.indexGet);
 /* GET a quote. */
 router.get('/wikiquote', indexes.wikiquoteGet);
 
+/* GET a quote. */
+router.get('/quote', indexes.quoteGet);
+
+/* GET a quote. */
+router.get('/addwire', indexes.addwireGet);
+
 /* GET Affichage de la page d'erreur 404 */
 router.get('/erreur404', indexes.erreur404);
 
